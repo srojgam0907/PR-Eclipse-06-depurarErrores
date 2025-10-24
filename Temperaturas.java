c36a2c412ad98051c77157eb3b6b7659573bff0d
Sara rojas <srojgam0907@g.educaand.es>
2025-10-24 18:34:19 +0200

    Update Temperaturas.java

--- Temperaturas.java
+++ Temperaturas.java
@@ -7,24 +7,6 @@ import java.util.Scanner;
  * Después, repite mientras el usuario quiera continuar.
  */
 
-//RESPUESTA AL APARTADO 2 Que ocurre y como solucionarlo
-/*En la función pedirTemperatura el codigo nunca va a entrar en el if y nunca se va a repetir el bucle 
- *por que el operador logico que usa es un && (AND) y un valor no puede ser a la vez menor que -10 y mayor que 50.
- *Lo he arreglado cambiando el operador lógico por un || (OR) 
- *
- *En el main, en el bucle while, el if comprueba si la respuesta del usuario es n o N para tener en cuenta la mayuscula y 
- *aun así aunque escribas una n por teclado no entra en el if y no termina el programa por que esta comparando dos strings 
- *con el operador == y este solo comparar la referencia a la que apunta en memoria. Hay que hacerlo con .equals para que 
- *compare el contenido en sí de la variable. En este caso he usado .equalsIgnoreCase para no poner dos veces lo mismo y que 
- *no importe si es mayúscula o minúscula.
- **/
-
-//RESPUESTA AL APARTADO 3 ¿Piensas que la documentación de un programa es necesaria?
-/*Aunque sea una parte aburrida de programar, la documentación es necesaria para entender
- *lo que hace el código y cómo funciona, sobretodo cuando no lo has escrito tú. También es
- *muy importante para mejorar la legibilidad y que si un programador quiere mirar un código
- *sepa qué hace de forma rápida leyendo los comentarios.*/
-
 public class Temperaturas {
 
     public static void main(String[] args) {
@@ -91,3 +73,4 @@ public class Temperaturas {
         }
     }
 }
+
